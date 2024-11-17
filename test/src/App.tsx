55b68123986ff9abe3bd10a0bf4afb7b7045af44
Nick-njilla653 <ntchagadick@icloud.com>
2024-11-17 22:26:54 +0100

    VoterRegistrationModifié

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -3,6 +3,7 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import GestionBureauxVote from './GestionBureauxVote/GestionBureauxVote';
+import VoterRegistration from './VoterRegistration/VoterRegistration';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -42,6 +43,7 @@ const App: React.FC = () => (
       <IonRouterOutlet>
         <Route path="/home" component={Home} exact />
         <Route path="/GestionBureauxVote" component={GestionBureauxVote} exact />
+        <Route path="/VoterRegistration" component={VoterRegistration} exact />
         <Redirect exact from="/" to="/home" />
       </IonRouterOutlet>
     </IonReactRouter>
